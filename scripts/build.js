8370c379c06338986c34a46cec80f001e28dc94c
John Livingston <git@john-livingston.fr>
2022-08-09 11:24:20 +0200

    Using Esbuild to build typescript.

--- scripts/build.js
+++ scripts/build.js
@@ -2,14 +2,14 @@ const path = require('path')
 const esbuild = require('esbuild')
 
 const clientFiles = [
-  'video-watch.js'
+  'video-watch.ts'
 ]
 
 const configs = clientFiles.map(f => ({
-  entryPoints: [ path.resolve(__dirname, '..', 'build/client', f) ],
+  entryPoints: [ path.resolve(__dirname, '..', 'client', f) ],
   bundle: true,
   minify: true,
-  sourcemap: false, // FIXME: `true` does not work, because peertube does not serve sttic files.
+  sourcemap: false, // FIXME: `true` does not work for now, because peertube does not serve static files.
   format: 'esm',
   target: 'safari11',
   outfile: path.resolve(__dirname, '..', 'dist/client', f),
