fa02504be9c0de5789fb575d1ebb6569588260cf
John Livingston <git@john-livingston.fr>
2022-08-15 21:02:20 +0200

    Modal WIP.

--- client/video-watch.ts
+++ client/video-watch.ts
@@ -7,22 +7,41 @@ interface VideoWatchLoadedHookOptions {
   playlist?: any
 }
 
-async function addButton ({ video }: VideoWatchLoadedHookOptions): Promise<void> {
+function register (clientOptions: RegisterClientOptions): void {
+  const registerHook = clientOptions.registerHook
+
+  registerHook({
+    target: 'action:video-watch.video.loaded',
+    handler: async (videoWatchHookOptions: VideoWatchLoadedHookOptions) => {
+      await addButton(clientOptions, videoWatchHookOptions)
+    }
+  })
+
+  registerHook({
+    target: 'action:router.navigation-end',
+    handler: cleanDom
+  })
+}
+
+async function addButton (options: RegisterClientOptions, { video }: VideoWatchLoadedHookOptions): Promise<void> {
   console.log('[open-on-my-instance] addButton')
   if (video.isLocal) {
     return
   }
   // The video is remote, we must add the button.
-  console.log('[open-on-my-instance] The current video is remote, adding the button...')
-  // FIXME: use a Peertube placeholder
-  const openButton = document.createElement('a')
-  openButton.textContent = 'Open'
-  openButton.classList.add('icon-text')
+  setTimeout(() => {
+    console.log('[open-on-my-instance] The current video is remote, adding the button...')
+    // FIXME: use a Peertube placeholder
+    const openButton = document.createElement('a')
+    openButton.textContent = 'Open'
+    openButton.classList.add('action-button')
+    openButton.onclick = () => openModal(options, video)
 
-  const placeholder = document.getElementById('plugin-placeholder-player-next')
-  placeholder?.append(openButton)
+    const placeholder = document.getElementsByTagName('my-video-rate')[0]
+    placeholder?.before(openButton)
 
-  console.log('[open-on-my-instance] The button was added.')
+    console.log('[open-on-my-instance] The button was added.')
+  }, 100)
 }
 
 function cleanDom (): void {
@@ -30,17 +49,11 @@ function cleanDom (): void {
   // TODO: remove buttons?
 }
 
-function register ({
-  registerHook
-}: RegisterClientOptions): void {
-  registerHook({
-    target: 'action:video-watch.video.loaded',
-    handler: addButton
-  })
-
-  registerHook({
-    target: 'action:router.navigation-end',
-    handler: cleanDom
+function openModal ({ peertubeHelpers }: RegisterClientOptions, _video: Video): void {
+  peertubeHelpers.showModal({
+    title: 'Open on my instance',
+    content: '<p>Here we go!</p>',
+    close: true
   })
 }
 
