eecc608061ef4c6fd6f77d7f35a9ca578fb946ca
John Livingston <git@john-livingston.fr>
2022-09-26 21:32:27 +0200

    Trying to proxify...

--- modal/modal.ts
+++ modal/modal.ts
@@ -2,5 +2,9 @@ import { init } from 'open-on-my-instance'
 
 const container: HTMLDivElement = document.querySelector('div[container]') as HTMLDivElement
 if (container) {
-  init(container).then(() => {}, () => {})
+  init(container, {
+    instancesAPI: {
+      listEndpoint: window.location.href.replace(/\/modal\/content\b/, '/modal/instances')
+    }
+  }).then(() => {}, () => {})
 }
--- server/main.ts
+++ server/main.ts
@@ -1,6 +1,8 @@
 import * as path from 'path'
 import type { RegisterServerOptions } from '@peertube/peertube-types'
 
+const got = require('got')
+
 async function register (options: RegisterServerOptions): Promise<any> {
   const peertubeHelpers = options.peertubeHelpers
   peertubeHelpers.logger.info('Registering the plugin...')
@@ -20,10 +22,33 @@ async function register (options: RegisterServerOptions): Promise<any> {
     res.sendFile(path.resolve(__dirname, '..', 'modal', 'modal.js.map'))
   })
 
+  router.get('/modal/instances', (req, res, next) => {
+    const f = async (): Promise<void> => {
+      // eslint-disable-next-line @typescript-eslint/no-base-to-string
+      const start = (req.query.start ?? '0').toString()
+      // eslint-disable-next-line @typescript-eslint/no-base-to-string
+      const count = (req.query.count ?? '100').toString()
+      peertubeHelpers.logger.debug('modal/instances: ' + start + '->' + start + '+' + count)
+      const result = await getPeertubeInstances(options, start, count)
+      res.type('json')
+      res.send(JSON.stringify(result))
+    }
+
+    f().catch(err => next(err))
+  })
+
   registerSettings(options)
 }
 
 function registerSettings (options: RegisterServerOptions): void {
+  options.registerSetting({
+    private: true,
+    type: 'html',
+    label: 'Documentation',
+    // eslint-disable-next-line max-len
+    descriptionHTML: 'You can find the documentation for this plugins <a target="_blank" href="https://github.com/JohnXLivingston/peertube-plugin-open-on-my-instance/blob/master/README.md">here</a>.'
+  })
+
   options.registerSetting({
     private: false,
     type: 'input',
@@ -31,9 +56,52 @@ function registerSettings (options: RegisterServerOptions): void {
     label: 'Iframe URL',
     descriptionHTML: 'The iframe to display in the dialog. Please read the documentation.'
   })
+
+  options.registerSetting({
+    private: true,
+    type: 'input',
+    name: 'directory-url',
+    label: 'Directory URL',
+    descriptionHTML: 'The url to the Peertube instances directory to use.',
+    default: 'https://instances.joinpeertube.org/api/v1/instances'
+  })
 }
 
 async function unregister (): Promise<any> {
+}
+
+interface PeertubeInstances {
+  total: number
+  data: Array<{
+    host: string
+    name: string
+  }>
+}
+
+async function getPeertubeInstances (
+  options: RegisterServerOptions, start: string, count: string
+): Promise<PeertubeInstances> {
+  if (!/^\d+$/.test(start)) {
+    throw new Error('Invalid start param')
+  }
+  if (!/^\d+$/.test(count)) {
+    throw new Error('Invalid count param')
+  }
+
+  const directoryUrl = await options.settingsManager.getSetting('directory-url')
+  if (!directoryUrl || typeof directoryUrl !== 'string') {
+    throw new Error('The directory is not configured.')
+  }
+
+  const url = new URL('', directoryUrl)
+  url.searchParams.set('start', start.toString())
+  url.searchParams.set('count', count.toString())
+
+  const result: PeertubeInstances = await got(directoryUrl, {
+    json: true
+  })
+
+  return result
 }
 
 module.exports = {
